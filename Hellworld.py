d7d2fc6de2005faa0e0ad64beeadd2a430141f1b
Porndet Pipitsuntonsan <likeink2@hotmail.com>
2021-10-29 15:10:24 +0700

    initial comment

--- Hellworld.py
+++ Hellworld.py
@@ -1,3 +1,18 @@
-for x in range(5):
-    print(x)
-    print(x)
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con
+
+while keyboard.is_pressed('q') == False:
+    pic = pyautogui.screenshot(region=(660,350,600,400))
+    width, height = pic.size
+
+    for x in range(0,width,5):
+        for y in range(0,height,5):
+            r,g,b = pic.getpixel((x,y))
+            if b == 195:
+                pyautogui.click(x+width,h+height)
+                time.sleep(0.1)
+                break
