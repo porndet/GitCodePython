f7ae0bdac3f1b3699c835cc060181041f1790181
Porndet Pipitsuntonsan <40681502+porndet@users.noreply.github.com>
2021-11-15 15:55:51 +0700

    Add files via upload

--- ColorDectection.py
+++ ColorDectection.py
@@ -1,10 +1,27 @@
 import cv2 
 import numpy as np
+import pyautogui
 
+# px = pyautogui.pixel(1500, 1000)
+# print(px)
+im = pyautogui.screenshot(region=(0,0,100,100))
+capture = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
+cv2.imshow("Result",capture)
+
+# for x in range(0,100,5) :
+#     for y in range(0,100,5) :
+#         r, g, b = pyautogui.pixel(x, y)
+#         print(g)
+
+x, y = pyautogui.locateCenterOnScreen('logoVisualStudioCode.png', grayscale=True, confidence=0.7) 
+pyautogui.moveTo(x, y)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#Amazon Textract with Python
+#PyTesseract: Python Optical Character Recognition | Using Tesseract OCR with Python
 # img = cv2.imread("color-signals.jpg")
-
 # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
 # lower_green = np.array([36, 25, 25])
 # upper_green = np.array([70, 255, 255])
 # mask1 = cv2.inRange(hsv, lower_green, upper_green)
@@ -17,19 +34,3 @@ import numpy as np
 
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
-
-# mouse callback function
-def draw_circle(event,x,y,flags,param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(img,(x,y),100,(255,0,0),-1)
-
-# Create a black image, a window and bind the function to window
-img = np.zeros((512,512,3), np.uint8)
-cv2.namedWindow('image')
-cv2.setMouseCallback('image',draw_circle)
-
-while(1):
-    cv2.imshow('image',img)
-    if cv2.waitKey(20) & 0xFF == 27:
-        break
-cv2.destroyAllWindows()
